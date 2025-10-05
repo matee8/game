354a4b7f0b9f813cdbed606103c1f991444e2836
Gunics Roland <grolcsi09@gmail.com>
2025-10-05 21:38:23 +0200

    fix: player struct now with small p

--- src/player.c
+++ src/player.c
@@ -1,20 +1,26 @@
 #include "../include/game/player.h"
+#include <raylib.h>
 
-void InitPlayer(Player* player, Vector3 position, float speed, int health, Model model) {
+void init_player(player* player, Vector3 position, float speed, int health) {
+    Model model = LoadModel("assets/models/player/player.glb");
+    Texture2D texture = LoadTexture("assets/textures/player/player.png");
+    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
+
     player->position = position;
     player->speed = speed;
     player->health = health;
-    player->direction = 0; // Default direction
+    player->direction = 0;
     player->model = model;
+    player->texture = texture;
 }
 
-void UpdatePlayer(Player* player) {
-    // Update player position based on input
-    if (IsKeyDown(KEY_W)) player->position.z -= player->speed;
-    if (IsKeyDown(KEY_S)) player->position.z += player->speed;
-    if (IsKeyDown(KEY_A)) player->position.x -= player->speed;
-    if (IsKeyDown(KEY_D)) player->position.x += player->speed;
-
-    // Update player model position
-    //player->model.transform.position = player->position;
+void update_player(player* player) {
+    if (IsKeyDown(KEY_W))
+        player->position.z -= player->speed;
+    if (IsKeyDown(KEY_S))
+        player->position.z += player->speed;
+    if (IsKeyDown(KEY_A))
+        player->position.x -= player->speed;
+    if (IsKeyDown(KEY_D))
+        player->position.x += player->speed;
 }
