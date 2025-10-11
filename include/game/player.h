97432cf01afa95510c57ba68742317c59e9e2203
Gunics Roland <grolcsi09@gmail.com>
2025-10-11 20:15:59 +0200

    fix: moved enum back to their own modules as per request

--- include/game/player.h
+++ include/game/player.h
@@ -24,6 +24,34 @@
 #define NUM_DIRECTIONS 8
 
 /**
+ * @enum direction
+ * @brief Represents compass directions for navigation or orientation.
+ */
+enum direction {
+    WEST,       /**< West direction */
+    EAST,       /**< East direction */
+    NORTH,      /**< North direction */
+    SOUTH,      /**< South direction */
+    NORTH_EAST, /**< North-East direction */
+    NORTH_WEST, /**< North-West direction */
+    SOUTH_EAST, /**< South-East direction */
+    SOUTH_WEST, /**< South-West direction */
+};
+
+/**
+ * @enum player_state
+ * @brief Represents the various states a player can be in.
+ */
+enum player_state {
+    STATE_IDLE,      /**< Player is idle, not performing any action */
+    STATE_RUNNING,   /**< Player is running */
+    STATE_ATTACKING, /**< Player is attacking */
+    STATE_RELOADING, /**< Player is reloading their weapon */
+    STATE_DEAD       /**< Player is dead */
+};
+
+
+/**
  * @struct player
  * @brief Represents a player in the 3D environment.
  *
