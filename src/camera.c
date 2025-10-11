b6c60e3649104bb3a4b1052385a94db1ad37e13d
matee8 <graves-bluff-pesky@duck.com>
2025-10-11 20:09:35 +0200

    refactor(camera): decouple camera from room system

--- src/camera.c
+++ src/camera.c
@@ -1,7 +1,6 @@
 #include "game/camera.h"
-#include "game/room.h"
 
-#include <stdlib.h>
+#include "raymath.h"
 
 void init_camera(struct camera* camera) {
     camera->camera_m.position = (Vector3){0.0F, 15.0F, 15.0F};
@@ -10,41 +9,36 @@ void init_camera(struct camera* camera) {
     camera->camera_m.fovy = 45.0F;
     camera->camera_m.projection = CAMERA_PERSPECTIVE;
 
-    camera->prev_current_room = NULL;
-    camera->prev_position = camera->camera_m.position;
     camera->target_position = camera->camera_m.position;
-    camera->prev_center = camera->camera_m.target;
     camera->target_center = camera->camera_m.target;
 }
 
-void update_camera(struct camera* camera, struct room* current_room) {
-    if (camera->prev_current_room != current_room) {
-        camera->prev_center = camera->target_center;
-        camera->target_center = get_center(current_room);
+void camera_focus_on(struct camera* camera, Vector3 new_center) {
+    if (camera->target_center.x != new_center.x ||
+        camera->target_center.y != new_center.y ||
+        camera->target_center.z != new_center.z) {
+        camera->target_center = new_center;
 
         camera->target_position = (Vector3){
-            camera->target_center.x + 0.0F,  // oldalról + kicsit felülről
-            camera->target_center.y + 15.0F, camera->target_center.z + 15.0F};
-
-        camera->prev_current_room = current_room;
+            .x = camera->target_center.x + 0.0F,
+            .y = camera->target_center.y + 15.0F,
+            .z = camera->target_center.z + 15.0F,
+        };
     }
 
-    float lerp = 0.1F;
+    float lerp_factor = 0.1F;
 
-    camera->prev_position.x +=
-        (camera->target_position.x - camera->prev_position.x) * lerp;
-    camera->prev_position.y +=
-        (camera->target_position.y - camera->prev_position.y) * lerp;
-    camera->prev_position.z +=
-        (camera->target_position.z - camera->prev_position.z) * lerp;
+    camera->camera_m.position.x = Lerp(camera->camera_m.position.x,
+                                       camera->target_position.x, lerp_factor);
+    camera->camera_m.position.y = Lerp(camera->camera_m.position.y,
+                                       camera->target_position.y, lerp_factor);
+    camera->camera_m.position.z = Lerp(camera->camera_m.position.z,
+                                       camera->target_position.z, lerp_factor);
 
-    camera->prev_center.x +=
-        (camera->target_center.x - camera->prev_center.x) * lerp;
-    camera->prev_center.y +=
-        (camera->target_center.y - camera->prev_center.y) * lerp;
-    camera->prev_center.z +=
-        (camera->target_center.z - camera->prev_center.z) * lerp;
-
-    camera->camera_m.position = camera->prev_position;
-    camera->camera_m.target = camera->prev_center;
+    camera->camera_m.target.x =
+        Lerp(camera->camera_m.target.x, camera->target_center.x, lerp_factor);
+    camera->camera_m.target.y =
+        Lerp(camera->camera_m.target.y, camera->target_center.y, lerp_factor);
+    camera->camera_m.target.z =
+        Lerp(camera->camera_m.target.z, camera->target_center.z, lerp_factor);
 }
