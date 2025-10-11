6367a8b4a92a853f54ab4340c396b2330537a923
Gunics Roland <grolcsi09@gmail.com>
2025-10-11 13:46:07 +0200

    chore: moved enums into separeta header

---
+++ include/game/enum.h
@@ -0,0 +1,24 @@
+#ifndef ENUM_H
+#define ENUM_H
+
+enum neighbor {
+    RIGHT,   /**< Neighbor to the right */
+    LEFT,    /**< Neighbor to the left */
+    UP,      /**< Neighbor above */
+    DOWN,    /**< Neighbor below */
+    FORWARD, /**< Neighbor in front */
+    BACKWARD /**< Neighbor behind */
+};
+
+enum direction{
+    WEST,
+    EAST,
+    NORTH,
+    SOUTH,
+    NORTH_EAST,
+    NORTH_WEST,
+    SOUTH_EAST,
+    SOUTH_WEST,
+};
+
+#endif
